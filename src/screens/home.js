6b13f5d8378497e9c7d0e6f187b68e212b8aafed
AlamGarry <alqifariq@gmail.com>
2024-06-10 12:34:24 +0700

    adding delete alert

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,35 +1,48 @@
 import React from 'react';
-import { FlatList, StyleSheet, View, Text } from 'react-native';
+import { FlatList, StyleSheet, View, Text, Alert } from 'react-native';
 import CustomButton from '../components/customButton';
 
-const NoteCard = ({ item, deleteNote, findNote }) => (
-  <View style={styles.card}>
-    <Text style={styles.cardTitle}>{item.title}</Text>
-    <Text>{item.desc}</Text>
-    <View style={styles.buttons}>
-      <CustomButton
-        backgroundColor='#FFC300'
-        color='#151D3B'
-        text='Ubah'
-        fontSize={12}
-        width={100}
-        onPress={() => {
-          findNote(item.id);
-        }}
-      />
-      <CustomButton
-        backgroundColor='#D82148'
-        color='#fff'
-        text='Hapus'
-        fontSize={12}
-        width={100}
-        onPress={() => {
-          deleteNote(item.id);
-        }}
-      />
-    </View>
-  </View>
-);
+const NoteCard = ({ item, deleteNote, findNote }) =>
+  (() => {
+    const alertDelete = () => {
+      Alert.alert('Hapus Note', 'Apakah anda yakin ingin menghapus note ini?', [
+        {
+          text: 'Batal',
+          onPress: () => {},
+        },
+        { text: 'Ya', onPress: () => deleteNote(item.id) },
+      ]);
+    };
+
+    return (
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>{item.title}</Text>
+        <Text>{item.desc}</Text>
+        <View style={styles.buttons}>
+          <CustomButton
+            backgroundColor='#FFC300'
+            color='#151D3B'
+            text='Ubah'
+            fontSize={12}
+            width={100}
+            onPress={() => {
+              findNote(item.id);
+            }}
+          />
+          <CustomButton
+            backgroundColor='#D82148'
+            color='#fff'
+            text='Hapus'
+            fontSize={12}
+            width={100}
+            onPress={() => {
+              alertDelete();
+            }}
+          />
+        </View>
+      </View>
+    );
+  })();
 
 const Home = ({ noteList, setCurrentPage, deleteNote, findNote }) => {
   return (
